ab9614091cd561fefb4119b1096281f7e8dd3b1a
ayshe <ayshenko@gmail.com>
2023-04-02 11:39:31 +0300

    add file

---
+++ Изучаем Python 3.10 с основ, разработка на языке Python (2023)/Part 1 Рабочая среда и базовые понимания/lesson_1.py
@@ -0,0 +1,20 @@
+num = 4   # type int
+num_2 = 4.2  # type float
+Res = "Hello world"  # type str
+print(type(num))
+print(type(num_2))
+print(type(Res))
+
+print(num == 4)  # type булево
+print(num != 4)
+print(num >= 4)
+print(num <= 4)
+
+print(len(Res)) # Количесво символов
+
+print(Res.count("l")) # Посчитать количесво "l"
+print(Res.split())  # Разделить строку. По умолчанию разделитель пробел. Вывод :['Hello', 'world']
+
+
+
+
