e32b07eeb35c5dc5e7dcf1e35c780874690a7b72
artyom <ayshenko@gmail.com>
2023-04-16 11:36:47 +0300

    Урок условия

--- Изучаем Python 3.10 с основ, разработка на языке Python (2023)/Part 2 Условные операторы/lesson_2.py
+++ Изучаем Python 3.10 с основ, разработка на языке Python (2023)/Part 2 Условные операторы/lesson_2.py
@@ -1,1 +1,15 @@
 #Второй урок
+if True:
+    print("Hello")
+
+if 3>4:
+    print("No")  #Не будет вывода, т.юк. лож
+
+a = 3
+b = 4
+if a>b:
+    print("a>b")
+elif a == b:
+    print("a=b")
+else:
+    print("a<b")
