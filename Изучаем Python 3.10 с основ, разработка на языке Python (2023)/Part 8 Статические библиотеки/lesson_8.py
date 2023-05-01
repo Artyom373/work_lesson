753229e92e2ecba109b97978feb680213d8c2905
artyom <ayshenko@gmail.com>
2023-05-01 13:01:43 +0300

    Урок numpy

---
+++ Изучаем Python 3.10 с основ, разработка на языке Python (2023)/Part 8 Статические библиотеки/lesson_8.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Вектор с 10 нулями
+z = np.zeros(10)
+print(z)
+
+# Вектор с 8 единицами
+y = np.ones(8)
+print(y)
+
+# Вектор с 5 2.5
+t = np.full(5,2.5)
+print(t)
+
+# Создать массив
+m = np.array([1,2,3,4])
+print(m)
+
+# Создание определенного массива
+r = np.arange(10,50)
+print(r)
