bb9233af59189297a6d8c9c7751052fdbb2dadc6
artyom <ayshenko@gmail.com>
2023-05-01 10:52:50 +0300

    Урок Функции 2

--- Изучаем Python 3.10 с основ, разработка на языке Python (2023)/Part 6 Функции/lesson_6.py
+++ Изучаем Python 3.10 с основ, разработка на языке Python (2023)/Part 6 Функции/lesson_6.py
@@ -1,1 +1,25 @@
 #Функции
+def sey_hello():
+    print("Hello my dear")
+
+sey_hello()
+
+def sey_goodbye():
+    print("Goobye my dear")
+
+sey_goodbye()
+
+def mult(x):
+    print(x*x)
+
+mult(3)
+
+def sum(x=3):
+    print(x+x)
+
+sum()
+
+def ret(x):
+    return x*x #Возвращает значение
+
+print(ret(5))
